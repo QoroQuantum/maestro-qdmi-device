cb8f1878ea819f9d39ea6f6a4eeebf9d0b82a070
Adrian Roman <aroman_ro@yahoo.com>
2025-11-19 16:56:29 +0200

    This has more/better implementation, but not yet the functionality. The tests should pass now, though

--- src/maestro_device.cpp
+++ src/maestro_device.cpp
@@ -58,6 +58,9 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 #include <utility>
 #include <vector>
 
+
+#include "Simulator.hpp"
+
 enum class MAESTRO_QDMI_DEVICE_SESSION_STATUS : uint8_t { ALLOCATED, INITIALIZED };
 
 /**
@@ -66,9 +69,8 @@ enum class MAESTRO_QDMI_DEVICE_SESSION_STATUS : uint8_t { ALLOCATED, INITIALIZED
  */
 struct MAESTRO_QDMI_Device_Session_impl_d {
 	std::string token;
-	MAESTRO_QDMI_DEVICE_SESSION_STATUS status =
-		MAESTRO_QDMI_DEVICE_SESSION_STATUS::ALLOCATED;
-	int qubits_num = 16; // some reasonable default value
+	MAESTRO_QDMI_DEVICE_SESSION_STATUS status = MAESTRO_QDMI_DEVICE_SESSION_STATUS::ALLOCATED;
+	int qubits_num = 64; // some reasonable default value
 };
 
 /**
@@ -78,11 +80,11 @@ struct MAESTRO_QDMI_Device_Session_impl_d {
 struct MAESTRO_QDMI_Device_Job_impl_d {
 	MAESTRO_QDMI_Device_Session session = nullptr;
 	int id = 0;
-	QDMI_Program_Format format = QDMI_PROGRAM_FORMAT_MAX;
+	QDMI_Program_Format format = QDMI_PROGRAM_FORMAT_QASM2;
 	void* program = nullptr;
 	QDMI_Job_Status status = QDMI_JOB_STATUS_SUBMITTED;
 	size_t num_shots = 0;
-	int qubits_num = 16;
+	int qubits_num = 64;
 	//std::vector<std::string> results;
 	//std::vector<std::complex<double>> state_vec;
 };
@@ -180,6 +182,89 @@ namespace {
 		return state->dis_real(state->gen);
 	}
 
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE0{ 0 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE1{ 1 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE2{ 2 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE3{ 3 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE4{ 4 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE5{ 5 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE6{ 6 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE7{ 7 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE8{ 8 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE9{ 9 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE10{ 10 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE11{ 11 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE12{ 12 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE13{ 13 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE14{ 14 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE15{ 15 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE16{ 16 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE17{ 17 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE18{ 18 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE19{ 19 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE20{ 20 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE21{ 21 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE22{ 22 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE23{ 23 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE24{ 24 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE25{ 25 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE26{ 26 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE27{ 27 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE28{ 28 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE29{ 29 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE30{ 30 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE31{ 31 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE32{ 32 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE33{ 33 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE34{ 34 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE35{ 35 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE36{ 36 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE37{ 37 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE38{ 38 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE39{ 39 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE40{ 40 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE41{ 41 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE42{ 42 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE43{ 43 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE44{ 44 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE45{ 45 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE46{ 46 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE47{ 47 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE48{ 48 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE49{ 49 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE50{ 50 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE51{ 51 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE52{ 52 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE53{ 53 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE54{ 54 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE55{ 55 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE56{ 56 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE57{ 57 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE58{ 58 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE59{ 59 };
+
+	constexpr MAESTRO_QDMI_Site_impl_d SITE60{ 60 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE61{ 61 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE62{ 62 };
+	constexpr MAESTRO_QDMI_Site_impl_d SITE63{ 63 };
+
+
+	constexpr std::array<const MAESTRO_QDMI_Site_impl_d*, 64> MAESTRO_DEVICE_SITES = {
+		&SITE0, &SITE1, &SITE2, &SITE3, &SITE4, &SITE5, &SITE6, &SITE7, &SITE8, &SITE9,
+		&SITE10, &SITE11, &SITE12, &SITE13, &SITE14, &SITE15, &SITE16, &SITE17, &SITE18, &SITE19,
+		&SITE20, &SITE21, &SITE22, &SITE23, &SITE24, &SITE25, &SITE26, &SITE27, &SITE28, &SITE29,
+		&SITE30, &SITE31, &SITE32, &SITE33, &SITE34, &SITE35, &SITE36, &SITE37, &SITE38, &SITE39,
+		&SITE40, &SITE41, &SITE42, &SITE43, &SITE44, &SITE45, &SITE46, &SITE47, &SITE48, &SITE49,
+		&SITE50, &SITE51, &SITE52, &SITE53, &SITE54, &SITE55, &SITE56, &SITE57, &SITE58, &SITE59,
+		&SITE60, &SITE61, &SITE62, &SITE63
+	};
+
 } // namespace
 
 // NOLINTBEGIN(bugprone-macro-parentheses)
@@ -269,9 +354,9 @@ int MAESTRO_QDMI_device_session_init(MAESTRO_QDMI_Device_Session session) {
 	default:
 		break;
 	}
-	if (session->token.empty()) {
-		return QDMI_ERROR_PERMISSIONDENIED;
-	}
+	//if (session->token.empty()) {
+	//	return QDMI_ERROR_PERMISSIONDENIED;
+	//}
 	session->status = MAESTRO_QDMI_DEVICE_SESSION_STATUS::INITIALIZED;
 	return QDMI_SUCCESS;
 } /// [DOXYGEN FUNCTION END]
@@ -295,7 +380,7 @@ int MAESTRO_QDMI_device_session_set_parameter(MAESTRO_QDMI_Device_Session sessio
 	if (session->status != MAESTRO_QDMI_DEVICE_SESSION_STATUS::ALLOCATED) {
 		return QDMI_ERROR_BADSTATE;
 	}
-	if (param != QDMI_DEVICE_SESSION_PARAMETER_TOKEN || param != QDMI_DEVICE_SESSION_PARAMETER_CUSTOM1) {
+	if (param != QDMI_DEVICE_SESSION_PARAMETER_TOKEN && param != QDMI_DEVICE_SESSION_PARAMETER_CUSTOM1) {
 		return QDMI_ERROR_NOTSUPPORTED;
 	}
 	if (value != nullptr) {
@@ -357,10 +442,7 @@ int MAESTRO_QDMI_device_job_set_parameter(MAESTRO_QDMI_Device_Job job,
 				format != QDMI_PROGRAM_FORMAT_CUSTOM5) {
 				return QDMI_ERROR_INVALIDARGUMENT;
 			}
-			if (format != QDMI_PROGRAM_FORMAT_QASM2 &&
-				format != QDMI_PROGRAM_FORMAT_QIRBASESTRING &&
-				format != QDMI_PROGRAM_FORMAT_QIRBASEMODULE &&
-				format != QDMI_PROGRAM_FORMAT_CALIBRATION) {
+			if (format != QDMI_PROGRAM_FORMAT_QASM2) {
 				return QDMI_ERROR_NOTSUPPORTED;
 			}
 			job->format = format;
@@ -416,7 +498,7 @@ int MAESTRO_QDMI_device_job_query_property(MAESTRO_QDMI_Device_Job job,
 } /// [DOXYGEN FUNCTION END]
 
 int MAESTRO_QDMI_device_job_submit(MAESTRO_QDMI_Device_Job job) {
-	return QDMI_ERROR_NOTIMPLEMENTED;
+	return QDMI_ERROR_NOTSUPPORTED;
 }
 
 int MAESTRO_QDMI_device_job_cancel(MAESTRO_QDMI_Device_Job job) {
@@ -454,7 +536,26 @@ int MAESTRO_QDMI_device_job_wait(MAESTRO_QDMI_Device_Job job, const size_t timeo
 int MAESTRO_QDMI_device_job_get_results(MAESTRO_QDMI_Device_Job job,
 	QDMI_Job_Result result, const size_t size,
 	void* data, size_t* size_ret) {
-	return QDMI_ERROR_NOTIMPLEMENTED;
+	if (job == nullptr || job->status != QDMI_JOB_STATUS_DONE ||
+		(data != nullptr && size == 0) ||
+		(result >= QDMI_JOB_RESULT_MAX && result != QDMI_JOB_RESULT_CUSTOM1 &&
+			result != QDMI_JOB_RESULT_CUSTOM2 && result != QDMI_JOB_RESULT_CUSTOM3 &&
+			result != QDMI_JOB_RESULT_CUSTOM4 &&
+			result != QDMI_JOB_RESULT_CUSTOM5)) {
+		return QDMI_ERROR_INVALIDARGUMENT;
+	}
+
+	switch (result) {
+	case QDMI_JOB_RESULT_HIST_KEYS:
+	case QDMI_JOB_RESULT_HIST_VALUES:
+		//return MAESTRO_QDMI_device_job_get_results_hist(job, result, size, data,
+		//	size_ret);
+		break;
+	default:
+		break;
+	}
+
+	return QDMI_ERROR_NOTSUPPORTED;
 }
 
 int MAESTRO_QDMI_device_session_query_device_property(
@@ -474,9 +575,9 @@ int MAESTRO_QDMI_device_session_query_device_property(
 	}
 	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_NAME, "Maestro Device",
 		prop, size, value, size_ret);
-	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_VERSION, "0.1.0", prop, size, value,
+	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_VERSION, "0.0.1", prop, size, value,
 		size_ret);
-	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LIBRARYVERSION, "1.1.0", prop, size,
+	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LIBRARYVERSION, "0.0.1", prop, size,
 		value, size_ret);
 	ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_STATUS, QDMI_Device_Status,
 		MAESTRO_QDMI_get_device_status(), prop, size, value,
@@ -484,12 +585,13 @@ int MAESTRO_QDMI_device_session_query_device_property(
 
 	ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_QUBITSNUM, int, session->qubits_num, prop,
 		size, value, size_ret);
-	//        ADD_LIST_PROPERTY(QDMI_DEVICE_PROPERTY_SITES, MAESTRO_QDMI_Site, MAESTRO_DEVICE_SITES,
-	//            prop, size, value, size_ret);
+	
+	ADD_LIST_PROPERTY(QDMI_DEVICE_PROPERTY_SITES, MAESTRO_QDMI_Site, MAESTRO_DEVICE_SITES,
+	            prop, size, value, size_ret);
+	
 	//        ADD_LIST_PROPERTY(QDMI_DEVICE_PROPERTY_OPERATIONS, MAESTRO_QDMI_Operation,
 	//            MAESTRO_DEVICE_OPERATIONS, prop, size, value, size_ret);
-	//        ADD_LIST_PROPERTY(QDMI_DEVICE_PROPERTY_COUPLINGMAP, MAESTRO_QDMI_Site,
-	//            DEVICE_COUPLING_MAP, prop, size, value, size_ret);
+
 
 	// The example device never requires calibration
 	ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_NEEDSCALIBRATION, size_t, 0,
@@ -498,16 +600,6 @@ int MAESTRO_QDMI_device_session_query_device_property(
 	ADD_SINGLE_VALUE_PROPERTY(
 		QDMI_DEVICE_PROPERTY_PULSESUPPORT, QDMI_Device_Pulse_Support_Level,
 		QDMI_DEVICE_PULSE_SUPPORT_LEVEL_NONE, prop, size, value, size_ret);
-
-	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LENGTHUNIT, "um", prop, size, value,
-		size_ret);
-	ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_LENGTHSCALEFACTOR, double, 1.0,
-		prop, size, value, size_ret);
-
-	ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_DURATIONUNIT, "us", prop, size,
-		value, size_ret);
-	ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_DURATIONSCALEFACTOR, double,
-		0.001, prop, size, value, size_ret);
 
 	return QDMI_ERROR_NOTSUPPORTED;
 } /// [DOXYGEN FUNCTION END]
@@ -530,10 +622,7 @@ int MAESTRO_QDMI_device_session_query_site_property(MAESTRO_QDMI_Device_Session 
 		size, value, size_ret);
 	ADD_SINGLE_VALUE_PROPERTY(QDMI_SITE_PROPERTY_MODULEINDEX, uint64_t, 0, prop,
 		size, value, size_ret);
-	ADD_SINGLE_VALUE_PROPERTY(QDMI_SITE_PROPERTY_T1, uint64_t, 1000000U, prop,
-		size, value, size_ret);
-	ADD_SINGLE_VALUE_PROPERTY(QDMI_SITE_PROPERTY_T2, uint64_t, 100000000U, prop,
-		size, value, size_ret);
+
 	return QDMI_ERROR_NOTSUPPORTED;
 } /// [DOXYGEN FUNCTION END]
 
